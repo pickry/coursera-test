782c6fd0adb6169ef19ea2a2df29ed3e0073b1b3
pickry <adrija924@gmail.com>
2023-07-29 20:41:46 +0530

    error

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(function () {
     $("#navbarToggle").blur(function (event){
         var screenWidth = window.innerWidth;
-        if(screenWidth < 768){
+        if (screenWidth < 768){
             $("#collapsable-nav").collapse('hide'); 
         }
-    })
-})
+    });
+});
